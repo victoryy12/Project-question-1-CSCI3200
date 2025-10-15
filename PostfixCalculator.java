a477da0030bb9f2409e1802eb50dd8eef2166501
victoryy12 <victor3perez86@gmail.com>
2025-10-15 10:10:52 -0400

    Add files via upload

---
+++ PostfixCalculator.java
@@ -0,0 +1,9 @@
+import java.util.Stack;
+
+public class PostfixCalculator {
+    public static void PostfixToInfix(String post) {
+        Stack<Integer> stack = new Stack<>();
+        
+
+    }
+}
